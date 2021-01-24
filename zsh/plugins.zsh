654c4b021e9cefaa1c4ef7f6ea16e255c7cf8ee4
shima <shima.kazuhide0705@gmail.com>
2021-01-25 03:13:22 +0900

    delete cat ailias

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -9,6 +9,6 @@ zinit light zdharma/fast-syntax-highlighting
 zinit ice as"program" from"gh-r" mv"bat* -> bat" pick"bat/bat"
 zinit light sharkdp/bat
 #エイリアス設定
-if builtin command -v bat > /dev/null; then
-  alias cat="bat"
-fi
+#if builtin command -v bat > /dev/null; then
+#  alias cat="bat"
+#fi
