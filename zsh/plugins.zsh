9e0ae157afecab92c0e757ad4490b2e01d6216e7
shima <shima.kazuhide0705@gmail.com>
2021-02-22 16:54:59 +0900

    add some plugins

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -4,11 +4,16 @@ zinit ice depth=1; zinit light romkatv/powerlevel10k
 ### syntax highlight
 zinit light zdharma/fast-syntax-highlighting
 
+### autosuggestions
+zinit light zsh-users/zsh-autosuggestions
 
 ### bat
 zinit ice as"program" from"gh-r" mv"bat* -> bat" pick"bat/bat"
 zinit light sharkdp/bat
-#エイリアス設定
-#if builtin command -v bat > /dev/null; then
-#  alias cat="bat"
-#fi
+
+### enhancd
+zinit ice proto'git' pick'init.sh'
+zinit light b4b4r07/enhancd
+
+### cli-finder
+zinit light b4b4r07/cli-finder
